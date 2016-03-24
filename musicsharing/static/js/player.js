8a0224a29db81b6e5a11cf63340d51e0a520b1fe
Pengcheng Sun <pengches@andrew.cmu.edu>
2016-03-24 00:43:36 -0400

    bug fixes - user playlist js function

---
+++ musicsharing/static/js/player.js
@@ -0,0 +1,49 @@
+var ap4 = new APlayer({
+        element: document.getElementById('player4'),
+        narrow: false,
+        autoplay: false,
+        showlrc: false,
+        theme: '#ad7a86',
+        music: [{
+            title: 'Uptown Funk',
+            author: 'Mark Ronson',
+            url: 'http://127.0.0.1:8000/music/Uptown Funk',
+            pic: 'http://7xq131.com1.z0.glb.clouddn.com/secret base~.jpg'
+        }, {
+            title: '回梦游仙',
+            author: '骆集益',
+            url: 'http://127.0.0.1:8000/music/huimengyouxian',
+            pic: 'http://127.0.0.1:8000/picture/huimengyouxian'
+        }, {
+            title: 'Cry me a river',
+            author: 'Justin Timberlake',
+            url: 'http://7xq131.com1.z0.glb.clouddn.com/secret base~.mp3',
+            pic: 'http://7xq131.com1.z0.glb.clouddn.com/あっちゅ～ま青春!.jpg'
+        }, {
+            title: 'lie',
+            author: 'Bigbang',
+            url: 'http://7xq131.com1.z0.glb.clouddn.com/secret base~.mp3',
+            pic: 'http://7xq131.com1.z0.glb.clouddn.com/secret base~.jpg'
+        }, {
+            title: 'Cakes by the ocean',
+            author: 'DDDD',
+            url: 'http://7xq131.com1.z0.glb.clouddn.com/回レ！雪月花.mp3',
+            pic: 'http://7xq131.com1.z0.glb.clouddn.com/回レ！雪月花.jpg'
+        }, {
+            title: 'Cry me a river',
+            author: 'Justin Timberlake',
+            url: 'http://7xq131.com1.z0.glb.clouddn.com/secret base~.mp3',
+            pic: 'http://7xq131.com1.z0.glb.clouddn.com/あっちゅ～ま青春!.jpg'
+        }, {
+            title: 'lie',
+            author: 'Bigbang',
+            url: 'http://7xq131.com1.z0.glb.clouddn.com/secret base~.mp3',
+            pic: 'http://7xq131.com1.z0.glb.clouddn.com/secret base~.jpg'
+        }, {
+            title: 'Cakes by the ocean',
+            author: 'DDDD',
+            url: 'http://7xq131.com1.z0.glb.clouddn.com/回レ！雪月花.mp3',
+            pic: 'http://7xq131.com1.z0.glb.clouddn.com/回レ！雪月花.jpg'
+        }]
+    });
+    ap4.init();
