54ae6d6f7ef8598353a0f18b42b63888ce96c381
Yayun Huang <arainhyy@gmail.com>
2016-04-05 17:47:53 -0400

    finish automatic completing js function

--- musicsharing/static/js/pymusic.js
+++ musicsharing/static/js/pymusic.js
@@ -1,18 +1,23 @@
-document.getElementById("user-option").addEventListener("click", change_search_state);
-document.getElementById("song-option").addEventListener("click", change_search_state);
+
 
 $(document).ready(function() {
-	var user_option = $(".selecter-options"):first;
-	var song_option = $(".selecter-options"):last;
+	var user_option = $(".form-search .selecter-item:first");
+	var song_option = $(".form-search .selecter-item:last");
+	console.log(song_option.html());
 	user_option.attr('id', 'user-option');
 	song_option.attr('id', 'song-option');
+	document.getElementById("user-option").addEventListener("click", change_search_state);
+	document.getElementById("song-option").addEventListener("click", change_search_state);
 }); 
 
 function change_search_state() {
-	var state = $(".selecter-options").html();
+	var state = $(".selecter-selected").html();
+	console.log(state);
 	if (state == 'User') {
-		$.("id_search_user").hide();
+		$("#id_search_user").css("display","none");
+		$("#id_search_song").css("display","initial");
 	} else {
-		$.("id_search_song").hide();
+		$("#id_search_song").css("display","none");
+		$("#id_search_user").css("display","initial");	
 	}
 }
