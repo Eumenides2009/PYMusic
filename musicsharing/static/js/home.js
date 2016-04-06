e4b3d464299e68b72d5e501358230e35e2905165
Yayun Huang <arainhyy@gmail.com>
2016-04-06 03:26:21 -0400

    update layout of playlist.html: move songs successfully

--- musicsharing/static/js/home.js
+++ musicsharing/static/js/home.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var playlist = $("#playlist_choose");
-    console.log(playlist);
+    var parent = playlist.parent();
+    console.log(parent);
     $.ajax({
             url: "/get-list-name",
             type: "get",
@@ -9,14 +10,20 @@ $(document).ready(function() {
                 if (data['error'] != null) {
                     console.log("get-list-name error");
                 } else {
-                    var music = "";
                     var json = data['name'];
-                    console.log(json);
                     var html = "";
-                    for (var i = 1; i < json.length; i++) {
-                        html += '<span class="selecter-item selected" data-value="'+i+'">'+ json[i] + '</span>'
+                    var option_html = "";
+                    for (var i = 1; i <= json.length; i++) {
+                        if (i == 1) 
+                            html += '<span class="selecter-item selected" data-value="'+i+'">'+ json[i - 1] + '</span>';
+                        else
+                            html += '<span class="selecter-item" data-value="'+i+'">'+ json[i - 1] + '</span>';
+                        option_html += '<option value="'+i+'">'+ json[i - 1] + '</option>'
                     }
-                    playlist.append(html);
+                    console.log(parent.children());
+                    parent.children().eq(0).html(option_html);
+                    parent.children().eq(1).html(json[0]);
+                    parent.children().eq(2).html(html);
                 }
             },
             error: function() {
